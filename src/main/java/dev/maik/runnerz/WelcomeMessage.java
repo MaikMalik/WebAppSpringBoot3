4f4bb17903409d2d28b8c859e10f67fe141f1cd1
MaikDev <ibalov15@gmail.com>
2024-06-04 22:20:17 +0200

    message

--- src/main/java/dev/maik/runnerz/WelcomeMessage.java
+++ src/main/java/dev/maik/runnerz/WelcomeMessage.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class WelcomeMessage {
     public String getWelcomeMessage(){
-        return "Welcome to the Spring Boot Application";
+        return "Welcome to the Spring Boot Application it's the init";
     }
 }
--- src/main/java/dev/maik/runnerz/run/RunNotFoundException.java
+++ src/main/java/dev/maik/runnerz/run/RunNotFoundException.java
@@ -3,6 +3,8 @@ package dev.maik.runnerz.run;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class RunNotFoundException extends RuntimeException{
     public RunNotFoundException(){
